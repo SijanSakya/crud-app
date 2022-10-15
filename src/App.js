60e92b90acca9d4eeb2dd28b55cd8f38ebe48e77
pranay.bajracharya <law.pran99@gmail.com>
2022-10-15 14:54:43 +0545

    form validation submission

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-
-import './App.css';
-import Home from './components/Home';
-import { BrowserRouter as Router,  Routes,  Route  } from 'react-router-dom';
-import Add from './components/Add';
-import Edit from './components/Edit';
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
 //import Register from './components/Register';
-import Login from './components/Login';
-
+import Login from "./components/Login";
+import Employees from "./components/Employees";
+import { useState } from "react";
 
 function App() {
+  const [employee, setEmployee] = useState(Employees);
+
+  console.log(employee);
   return (
     <>
-     <div>
-     
-      <Router>
-        <Routes>
-        
-          <Route path ="/" element={<Home/>}> </Route>
-          <Route path ="/create" element={<Add/>}></Route>
-          <Route path ="/edit" element={<Edit/>}></Route>
-        
-        
-          
-          
-        </Routes>
-      </Router>
-     </div>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home employeeList={employee} />}></Route>
+            <Route
+              path="/create"
+              element={<Add setEmployee={setEmployee} />}
+            ></Route>
+            <Route path="/edit" element={<Edit />}></Route>
+          </Routes>
+        </Router>
+      </div>
     </>
   );
 }
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,122 +1,147 @@
 //rafce
-import React, {useState} from 'react'
-import  'bootstrap/dist/css/bootstrap.min.css';
-import {Button , Form} from 'react-bootstrap';
-import Employees from './Employees';
-import {v4 as uuid} from "uuid";
-import {Link , useNavigate} from 'react-router-dom';
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
+import { v4 as uuid } from "uuid";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm, useFormContext } from "react-hook-form";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup"
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const Add = () => {
-    const [FirstName, setFirstName ] =useState('');
-    const [LastName, setLastName ] =useState('');
-    const [Age, setAge ] =useState('');
-    const [Email, setEmail] =useState('');
-    const [Salary, setSalary ] =useState('');
-    const [Date, setDate ] =useState('');
+const Add = (props) => {
+  const { setEmployee } = props;
 
-
-    const schema =yup.object().shape({
-        formFirstName : yup.string().required("zz"),
-        formlastName : yup.string().required(),
-        formEmail : yup.string().email().required(),
-        formAge : yup.number().positive(). integer().required(),
-        formSalary : yup.number().positive(). integer().required(),
-        formDate : yup.date().required()
+  const schema = yup.object().shape({
+    formFirstName: yup.string().required("zz"),
+    formLastName: yup.string().required(),
+    formEmail: yup.string().email().required(),
+    formAge: yup.number().positive().integer().required(),
+    formSalary: yup.number().positive().integer().required(),
+    formDate: yup.date().required(),
   });
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema),
- } );
-    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
- const doSubmit =(data)=>{
+  // const { register } = useFormContext(); // retrieve all hook methods
+
+  const doSubmit = (data) => {
     console.log(data);
-}
-    
-    let history = useNavigate();
+  };
 
-    // const handleSubmits =(e)=>{
-    //    e.preventDefault();
+  let history = useNavigate();
 
-    //   const ids= uuid();
-    //   let uniqueId= ids.slice(0,8);
-    //    let a = FirstName,
-    //      b = LastName,
-    //      c = Age,
-    //      d= Email,
-    //      f=Salary, 
-    //      g=Date;
+  const onSubmit = (data) => {
+    // e.preventDefault();
 
-    //    Employees.push({id: uniqueId, FirstName: a ,LastName: b, Age:c, Email:d, Salary:f, Date:g});
-    //    history("/");
-    // }
+    console.log("working", data);
 
-    
+    const ids = uuid();
+    let uniqueId = ids.slice(0, 8);
+
+    // const date = new Date(data.formDate);
+
+    const newEmployee = {
+      id: uniqueId,
+      FirstName: data.formFirstName,
+      LastName: data.formLastName,
+      Age: data.formAge,
+      Email: data.formEmail,
+      Salary: data.formSalary,
+      Date: data.formDate.toLocaleDateString(),
+    };
+
+    setEmployee((prev) => {
+      return [...prev, newEmployee];
+    });
+    history("/");
+  };
+
+  const onError = (errors) => {
+    console.log("error", errors);
+  };
+
   return (
     <>
-    
-    <div>
-        <Form className="d-grid gap-2"  onSubmit={handleSubmit(doSubmit)} style = {{margin :"15rem"}}>
+      <div>
+        <Form
+          className="d-grid gap-2"
+          onSubmit={handleSubmit(onSubmit, onError)}
+          style={{ margin: "15rem" }}
+        >
+          <Form.Group className="mb-3" controlId="formFirstName">
+            <Form.Control
+              type="text"
+              placeholder="Enter First Name"
+              {...register("formFirstName")}
+              // onChange={(e) => setFirstName(e.target.value)}
+            ></Form.Control>
+            {errors.formFirstName?.message}
+          </Form.Group>
 
-            <Form.Group className = "mb-3" controlId="formFirstName">
-                <Form.Control type= "text" placeholder="Enter First Name" 
-                {...register("formFirstName")}
-                required onChange={(e) => setFirstName(e.target.value)}>
-                </Form.Control>
-                {errors.formFirstName?.message}
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formLastName">
+            <Form.Control
+              type="text"
+              placeholder="Enter Last Name"
+              {...register("formLastName")}
+              // onChange={(e) => setLastName(e.target.value)}
+            ></Form.Control>
+            {errors.formLastName?.message}
+          </Form.Group>
 
-            <Form.Group className = "mb-3" controlId="formLastName">
-                <Form.Control type= "text" placeholder="Enter Last Name"
-                 {...register("formLastName")}
-                required onChange={(e) => setLastName(e.target.value)}>
-                </Form.Control>
-                {errors.formLastName?.message}
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formAge">
+            <Form.Control
+              type="text"
+              placeholder="Enter Age"
+              {...register("formAge")}
+              // onChange={(e) => setAge(e.target.value)}
+            ></Form.Control>
+            {errors.formAge?.message}
+          </Form.Group>
 
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Control
+              type="text"
+              placeholder="Enter Email"
+              {...register("formEmail")}
+              // onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+            {errors.formEmail?.message}
+          </Form.Group>
 
-            <Form.Group className = "mb-3" controlId="formAge">
-                <Form.Control type= "text" placeholder="Enter Age"
-                   {...register("formAge")}
-                required onChange={(e) => setAge(e.target.value)}>
-                </Form.Control>
-                {errors.formAge?.message}
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formSalary">
+            <Form.Control
+              type="text"
+              placeholder="Enter Salary"
+              {...register("formSalary")}
+              // onChange={(e) => setSalary(e.target.value)}
+            ></Form.Control>
+            {errors.formSalary?.message}
+          </Form.Group>
 
-            <Form.Group className = "mb-3" controlId="formEmail">
-                <Form.Control type= "text" placeholder="Enter Email"
-                  {...register("formEmail")}
-                required onChange={(e) => setEmail(e.target.value)}>
-                </Form.Control>
-                {errors.formEmail?.message}
-            </Form.Group>
+          <Form.Group className="mb-3" controlId="formDate">
+            <Form.Control
+              type="date"
+              placeholder="Enter Date"
+              {...register("formDate")}
+              // onChange={(e) => setDate(e.target.value)}
+            ></Form.Control>
+            {errors.formDate?.message}
+          </Form.Group>
 
-            <Form.Group className = "mb-3" controlId="formSalary">
-                <Form.Control type= "text" placeholder="Enter Salary" 
-                  {...register("formSalary")}
-                required onChange={(e) => setSalary(e.target.value)}>
-                </Form.Control>
-                {errors.formSalary?.message}
-            </Form.Group>
-             
-            <Form.Group className = "mb-3" controlId="formDate">
-                <Form.Control type= "text" placeholder="Enter Date"
-                  {...register("formDate")}
-                required onChange={(e) => setDate(e.target.value)}>
-                </Form.Control>
-                {errors.formDate?.message}
-            </Form.Group>
-
-           
-            <Button type= "submit" >Create</Button>
-
+          {/* <Button type="submit" onClick={handleSubmit(onSubmit)}>
+            Create
+          </Button> */}
+          <input type="submit" value="Create" />
         </Form>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
